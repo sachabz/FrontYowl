0c4e4d781b6e4618a3889b0283fd4b5df01d6cbb
sacha <sacha.blin-zwertvaegher@epitech.eu>
2021-12-10 09:36:11 +0100

    First commit

---
+++ src/views/Profil.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <HeaderLogged />
+    <ProfilComponent />
+  </div>
+</template>
+
+
+
+<script>
+// @ is an alias to /src
+import HeaderLogged from "@/components/HeaderLogged.vue";
+import ProfilComponent from "@/components/Profil.vue";
+
+export default {
+  name: "Profil",
+  components: {
+    HeaderLogged,
+    ProfilComponent,
+  },
+};
+</script>
